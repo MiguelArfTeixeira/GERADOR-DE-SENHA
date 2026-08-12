c2d52046131e286862f461e4d1bb1a7b7662adc8
MiguelArfTeixeira <miguel.arf.teixeira@escola.pr.gov.br>
2026-08-12 16:06:12 -0300

    12/08

---
+++ main.js
@@ -0,0 +1,15 @@
+const numeroSenha= document.querySelector('.parametro-_texto');
+let tamanhoSenha=12;
+numeroSenha.textContent=tamanhoSenha;
+const botoes=document.querySelector('parametro-senha_botao');
+botoes[0].onclick=diminuiTamanho;
+botoes[1].onclick=aumentarTamanho
+function diminuiTamanho() {
+    tamanhoSenha=tamanhoSenha-1;
+    numeroSenha.textContent=tamanhoSenha;
+}
+
+function aumentarTamanho(){
+    tamanhoSenha=tamanhoSenha+2;
+    numeroSenha.textContent=tamanhoSenha;
+}
