21e67fe483334f1deb75873c95573fcda70cd97a
MiguelArfTeixeira <miguel.arf.teixeira@escola.pr.gov.br>
2026-08-19 16:12:44 -0300

    19/08

--- main.js
+++ main.js
@@ -2,19 +2,28 @@ const numeroSenha= document.querySelector('.parametro-_texto');
 let tamanhoSenha=12;
 numeroSenha.textContent=tamanhoSenha;
 const botoes=document.querySelectorAll('parametro-senha_botao');
-botoes[0].onclick=diminuiTamanho;
-if (tamanhoSenha>1) {
-       tamanhoSenha=tamanhoSenha-1;
-}
-botoes[1].onclick=aumentarTamanho
-if(tamanhoSenha<20){
- tamanhoSenha=tamanhoSenha+1;
-}
+botoes[0].onclick=diminuiTamanho();
+botoes[1].onclick=aumentarTamanho();
+
+
+
 function diminuiTamanho() {
- numeroSenha.textContent=tamanhoSenha;
+ if (tamanhoSenha>1) {
+tamanhoSenha--;
+}
+numeroSenha.textContent=tamanhoSenha;
 }
 
 function aumentarTamanho(){
-   
-    numeroSenha.textContent=tamanhoSenha;
+   if(tamanhoSenha<20){
+ tamanhoSenha++;
+}
+numeroSenha.textContent=tamanhoSenha;
+}
+
+const campoSenha=document.querySelector(#campo-senha);
+const letrasMaiusculas='ABCDEFGH';
+geraSenha()
+function geraSenha(){
+    let numeroAleatorio
 }
